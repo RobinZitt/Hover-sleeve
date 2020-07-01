de7555cfae5f12130b8b2413c39b9aa5b075dee2
RobinZitt <44872857+RobinZitt@users.noreply.github.com>
2020-07-01 19:00:20 +0200

    Update hover-sleeve.ino

--- hover-sleeve.ino
+++ hover-sleeve.ino
@@ -1,13 +1,11 @@
+unsigned char input1;
+
 void setup() {
   pinMode(11, OUTPUT);
-  pinMode(8, INPUT);
+  pinMode(3, INPUT_PULLUP);
 }
 
 void loop() {
-  if (digitalRead(8) == HIGH){
-    digitalWrite(11, HIGH);
-  }
-  else{
-    digitalWrite(11, LOW);
-  }
+  input1 = !digitalRead(3);
+  digitalWrite(11, input1);
 }
