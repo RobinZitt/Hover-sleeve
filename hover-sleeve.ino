7214c06e57e58cea28d7f8d5ff938bb20c1aace5
RobinZitt <44872857+RobinZitt@users.noreply.github.com>
2020-07-11 17:09:56 +0200

    Update hover-sleeve.ino
    Changed pins

--- hover-sleeve.ino
+++ hover-sleeve.ino
@@ -1,11 +1,11 @@
 unsigned char input1;
 
 void setup() {
-  pinMode(11, OUTPUT);
-  pinMode(3, INPUT_PULLUP);
+  pinMode(13, OUTPUT);
+  pinMode(11, INPUT_PULLUP);
 }
 
 void loop() {
-  input1 = !digitalRead(3);
-  digitalWrite(11, input1);
+  input1 = !digitalRead(11);
+  digitalWrite(13, input1);
 }
